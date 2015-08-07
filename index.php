3fde8e129ed4c4f5eee64d15a456adfd3b03437b
vietcavo <vietcavo@gmail.com>
2015-08-06 20:10:04 -0400

    entire site

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Headers</title>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <link rel="shortcut icon" href="common-files/icons/favicon.png">
+    <link rel="stylesheet" href="flat-ui/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="flat-ui/css/flat-ui.css">
+    <!-- Using only with Flat-UI (free)-->
+    <link rel="stylesheet" href="common-files/css/icon-font.css">
+    <!-- end -->
+    <link rel="stylesheet" href="common-files/css/animations.css">
+    <link rel="stylesheet" href="css/ui-kit-styles.css">
+  </head>
+  <body>
+    <div class="page-wrapper">
+      <!-- header-8 -->
+      <header class="header-8">
+        <div class="container">
+          <div class="row">
+            <div class="navbar col-sm-12" role="navigation">
+              <div class="navbar-header">
+                
+                <a class="brand" href="#">
+                  <img src="common-files/icons/logo.png" width="50" height="auto" alt="">Syncify</a>
+              </div>
+              <div class="collapse navbar-collapse">
+                <ul class="nav">
+     
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="header-background"></div>
+      </header>
+      <section class="header-8-sub bg-midnight-blue">
+        <div class="background">&nbsp;</div>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+              <h3>Join The Party Anywhere</h3>
+              <p class="lead">An app that lets you listen to music together, <u>live</u>, no matter where you are.<br>Coming Soon</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+              <img src="img/Iphone-6-Plus.png" width="320" height="auto" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+     
+    </div>
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="common-files/js/jquery-1.10.2.min.js"></script>
+    <script src="flat-ui/js/bootstrap.min.js"></script>
+    <script src="common-files/js/modernizr.custom.js"></script>
+    <script src="common-files/js/page-transitions.js"></script>
+    <script src="common-files/js/froogaloop.min.js"></script>
+    <script src="common-files/js/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script src="common-files/js/startup-kit.js"></script>
+  </body>
+</html>
